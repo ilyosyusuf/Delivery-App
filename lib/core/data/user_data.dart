6f9a5c72b2ee57827338d9821e441090328bd6d7
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-02-18 10:50:18 +0500

    Exam

---
+++ lib/core/data/user_data.dart
@@ -0,0 +1,8 @@
+import 'package:exam/models/authormodel/author.dart';
+
+class UserData{
+  static List<User> UserList = [
+    User(1, "John", "j@gmail.com","123456"),
+    User(2, "Lara", "l@gmail.com","654321"),
+  ];
+}
---
+++ lib/widgets/authorwidgets/sign_up_widget.dart
@@ -0,0 +1,19 @@
+import 'package:exam/core/components/sizeconfig/size_config.dart';
+import 'package:flutter/material.dart';
+
+class SignInWidget extends StatefulWidget {
+  const SignInWidget({ Key? key }) : super(key: key);
+
+  @override
+  _SignInWidgetState createState() => _SignInWidgetState();
+}
+
+class _SignInWidgetState extends State<SignInWidget> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Container(
+      width:
+    );
+  }
+}
